888ce695d39e3529173963d4f64fd0c1be8df7af
Amogh7 <amoghsachdeva3@gmail.com>
2021-01-12 23:17:30 +0530

    ready

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ io.on('connection',function(socket){
 
 });
 
-http.listen(3000,function(){
+
+let port=process.env.POST||3000;
+http.listen(port,function(){
     console.log('listening on :3000');
 });
